def5c428c48facd04c64b8d496d56ba2d8da9e49
Thien Nguyen <thienvu1013@gmail.com>
2020-11-26 16:50:10 -0700

    added

--- MySQL datadump/Datadump.sql
+++ MySQL datadump/Datadump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `InventoryApp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `InventoryApp`;
 -- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
 --
 -- Host: localhost    Database: InventoryApp
@@ -619,4 +621,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-26 16:39:50
+-- Dump completed on 2020-11-26 16:49:29
